af4ab6aa64eb5505726ed8794aab249d643f12b0
YuXiang Dong <yuxiang.dong@stonybrook.edu>
2022-12-27 23:36:16 -0500

    This is a test

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
+// void main(void)
+// {
+//     int number[12]; /* 12 cells, one cell per student */
+//     int index, sum = 0;
+
+//     /* Always initialize array before use */
+//     for (index = 0; index < 12; index++)
+//     {
+//         number[index] = index;
+//     }
+
+//     /* Now, number[index]=index; Error: Why? What kind?*/
+//     for (index = 0; index < 12; index = index + 1)
+//     {
+//         sum += number[index]; /* Sum array elements */
+//     }
+//     return;
+// }
